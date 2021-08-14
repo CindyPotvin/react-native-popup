dc050269ffc91d087cc4bc8c1985beae7e474531
potvcind <blog@cindypotvin.com>
2021-08-13 21:29:59 -0400

    Commiting the demo project

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Modal, Text, View, Pressable, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(true);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <Pressable style={styles.outsideModal}
+          onPress={(event) => { if (event.target == event.currentTarget) { 
+            setModalVisible(false); } }} >
+          <View style={styles.modal}>
+            <View style={styles.modalHeader}>
+              <View style={styles.modalHeaderContent}><Text>Other header content</Text></View>
+              <TouchableOpacity onPress={() => setModalVisible(false)}>
+                <Text style={styles.modalHeaderCloseText}>X</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.modalContent}>
+              <Text>
+                Popup content.
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+      </Modal>
     </View>
   );
 }
@@ -14,8 +34,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  modal: {
+    flex: 1,
+    margin: 50,
+    padding: 5,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  /* The content of the modal takes all the vertical space not used by the header. */
+  modalContent: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  modalHeader: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  /* The header takes up all the vertical space not used by the close button. */
+  modalHeaderContent: {
+    flexGrow: 1,
+  },
+  modalHeaderCloseText: {
+    textAlign: "center",
+    paddingLeft: 5,
+    paddingRight: 5
+  },
+  outsideModal: {
+    backgroundColor: "rgba(1, 1, 1, 0.2)",
+    flex: 1,
+  }
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+A example of a React Native popup that can be closed with a button. Please see this blog article for more details:
+https://blog.cindypotvin.com/how-to-close-a-react-native-modal-with-a-button/
